c8bc0117fb3c9949e6a4f38a931fa7b6cc641546
weeral <weeral@live.com>
2023-06-06 15:15:38 -0400

    episode 12 completed

--- src/main/java/org/example/gaming2d/tile/TileManager.java
+++ src/main/java/org/example/gaming2d/tile/TileManager.java
@@ -21,21 +21,64 @@ public class TileManager {
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        tile = new Tile[10];
+        tile = new Tile[50];
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
-        loadMap("/maps/world01.txt");
+        loadMap("/maps/worldV2.txt");
     }
 
     public void getTileImage() {
+            //PLACEHOLDERS
+            setUp(0, "grass00", false);
+            setUp(1, "grass00", false);
+            setUp(2, "grass00", false);
+            setUp(3, "grass00", false);
+            setUp(4, "grass00", false);
+            setUp(5, "grass00", false);
+            setUp(6, "grass00", false);
+            setUp(7, "grass00", false);
+            setUp(8, "grass00", false);
+            setUp(9, "grass00", false);
+            //PLACEHOLDERS
 
-            setUp(0, "grass", false);
-            setUp(1, "wall", true);
-            setUp(2, "water", true);
-            setUp(3, "earth", false);
-            setUp(4, "tree", true);
-            setUp(5, "sand", false);
+            setUp(10, "grass00", false);
+            setUp(11, "grass01", false);
+
+            setUp(12, "water00", true);
+            setUp(13, "water01", true);
+            setUp(14, "water02", true);
+            setUp(15, "water03", true);
+            setUp(16, "water04", true);
+            setUp(17, "water05", true);
+            setUp(18, "water06", true);
+            setUp(19, "water07", true);
+            setUp(20, "water08", true);
+            setUp(21, "water09", true);
+            setUp(22, "water10", true);
+            setUp(23, "water11", true);
+            setUp(24, "water12", true);
+            setUp(25, "water13", true);
+
+            setUp(26, "road00", false);
+            setUp(27, "road01", false);
+            setUp(28, "road02", false);
+            setUp(29, "road03", false);
+            setUp(30, "road04", false);
+            setUp(31, "road05", false);
+            setUp(32, "road06", false);
+            setUp(33, "road07", false);
+            setUp(34, "road08", false);
+            setUp(35, "road09", false);
+            setUp(36, "road10", false);
+            setUp(37, "road11", false);
+            setUp(38, "road12", false);
+
+            setUp(39, "earth", false);
+            setUp(40, "wall", true);
+            setUp(41, "tree", true);
+
+
     }
 
     public void setUp(int index, String imageName, boolean collision) {
